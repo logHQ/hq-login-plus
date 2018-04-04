7ea5fb88110b076cea77d64c5c3b600942f8df50
Anup Raj <anupofuniverse@gmail.com>
2018-04-04 17:09:43 +0545

    logo

--- hqloginplus.php
+++ hqloginplus.php
@@ -446,3 +446,27 @@ function hq_login_plus_logo_url_title() {
     
 }
 add_filter( 'login_headertitle', 'hq_login_plus_logo_url_title' );
+
+
+
+
+function hq_login_plus_login_head_img() {
+ 
+    if ( has_custom_logo() ) :
+ 
+        $image = wp_get_attachment_image_src( get_theme_mod( 'custom_logo' ), 'full' );
+        ?>
+        <style>
+            .login h1 a {
+                background-image: url(<?php echo esc_url( $image[0] ); ?>);
+                -webkit-background-size: <?php echo absint( $image[1] )?>px;
+                background-size: <?php echo absint( $image[1] ) ?>px;
+                height: <?php echo absint( $image[2] ) ?>px;
+                width: <?php echo absint( $image[1] ) ?>px;
+            }
+        </style>
+        <?php
+    endif;
+}
+ 
+add_action( 'login_head', 'hq_login_plus_login_head_img', 100 );
